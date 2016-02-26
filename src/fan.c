1aefef693e9a9bb4267de8aaba765576e46b1c5d
Jaska <aa>
2016-02-26 18:12:38 +0200

    test

--- src/fan.c
+++ src/fan.c
@@ -1,7 +1,7 @@
 #include "fan.h"
 
 void setFanImages(const char **paths, Fan *f, int count) {
-    f->images = new GtkImage[];
+    f->images = new GtkImage[count]*;
     f->imageCount = count;
     for (int i = 0; i < count; i++) {
         f->images[i] = gtk_image_new_from_file (paths[i]);
@@ -10,7 +10,7 @@ void setFanImages(const char **paths, Fan *f, int count) {
 
 void changeState(Fan *f) {
     if (NULL != f->button && NULL != f->images && f->imageCount > 0) {
-        gtk_button_set_image(f->button, f->images[f->currentImage])
+        gtk_button_set_image(f->button, (GtkWidget*)f->images[f->currentImage])
         f->currentImage++;
         if (f->currentImage >= f->imageCount) f->currentImage = 0;
     }
